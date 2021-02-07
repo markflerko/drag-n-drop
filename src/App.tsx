29992be1c20e80d0b00e6be237b2b4c92f4eb29e
markflerko <markflerko@gmail.com>
2021-02-07 14:17:41 +0300

    feat: add markup + figures

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <div id="grid">
+        <div className="figures">figures</div>
+        <div className="canvas" id="canvas">
+          canvas
+        </div>
+        <div className="figuresContainer" id="container">
+          <div className="hero circle draggable"></div>
+          <div className="hero square draggable"></div>
+        </div>
+        <div className="canvasContainer" id="canvasContainer">
+          <button id="delete">delete</button>
+        </div>
+      </div>
     </div>
   );
 }
